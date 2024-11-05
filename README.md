4ad406b766a96c695916b605682edcabe7851ff2
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-11-05 02:19:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# resynthesizer-rorted-offset
+
+The sorted offset code from [pdn-content-aware-fill](https://github.com/0xC0000054/pdn-content-aware-fill/blob/a4f282add238cd4fa4c26fd7149ee5f364288c93/src/Resynthesizer/Resynthesizer.cs#L427)
+extracted into a separate project to simplify performance tuning.
+
+## License
+
+This project is licensed under the terms of the GNU General Public License version 2.0.   
+See [License.txt](License.txt) for more information.
---
+++ src/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+namespace ResynthesizerSortedOffsetTest
+{
+    struct Point(int x, int y)
+    {
+        public int X = x;
+        public int Y = y;
+    }
+
+    internal readonly struct CartesianLessPointComparer : IComparer<Point>
+    {
+        public int Compare(Point point1, Point point2)
+        {
+            int point1Cartesian = (point1.X * point1.X) + (point1.Y * point1.Y);
+            int point2Cartesian = (point2.X * point2.X) + (point2.Y * point2.Y);
+
+            return point1Cartesian.CompareTo(point2Cartesian);
+        }
+    }
+
+    internal class Program
+    {
+        static List<Point> SortedOffsets(int sourceImageWidth, int sourceImageHeight)
+        {
+            ulong length = ((2 * (ulong)sourceImageWidth) - 1) * ((2 * (ulong)sourceImageHeight) - 1);
+
+            List<Point> offsets = new(checked((int)length));
+
+            for (int y = -sourceImageHeight + 1; y < sourceImageHeight; y++)
+            {
+                for (int x = -sourceImageWidth + 1; x < sourceImageWidth; x++)
+                {
+                    offsets.Add(new Point(x, y));
+                }
+            }
+
+            CartesianLessPointComparer comparer = new();
+
+            offsets.Sort(comparer.Compare);
+
+            return offsets;
+        }
+
+        static void Main()
+        {
+            const int SourceImageWidth = 7072;
+            const int SourceImageHeight = 6654;
+
+            Console.WriteLine("Sorting offsets for image size {0}x{1}...", SourceImageWidth, SourceImageHeight);
+
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+
+            List<Point> offsets = SortedOffsets(SourceImageWidth, SourceImageHeight);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Sorted {offsets.Count:n0} offsets in {stopwatch.ElapsedMilliseconds} ms.");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
